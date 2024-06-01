67a430da481265d0e81bf09df1f9efa860483f0d
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-06-02 05:52:01 +0600

    ok

--- src/Components/Home/Home/FlipCard.jsx
+++ src/Components/Home/Home/FlipCard.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components";
+import React from "react"
+import styled from "styled-components"
 
 const FlipCard = ({
   icon,
@@ -29,12 +29,12 @@ const FlipCard = ({
         <CardInfo color={infoColor}>{info}</CardInfo>
       </CardFront>
     </CardContainer>
-  );
-};
+  )
+}
 
 const CardContainer = styled.div`
   width: 100%;
-  height: ${({ isLast }) => (isLast ? "120px" : "150px")};
+  height: 150px;
   cursor: pointer;
   background-color: ${({ bgColor }) => bgColor};
   border-radius: 12px;
@@ -48,30 +48,37 @@ const CardContainer = styled.div`
   &:hover {
     transform: translateY(-5px);
   }
-`;
+
+  @media (max-width: 768px) {
+    height: ${({ isLast }) => (isLast ? "120px" : "150px")};
+  }
+`
 
 const CardFront = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
+`
 
 const CardIcon = styled.div`
-  font-size: ${({ isLast }) => (isLast ? "2rem" : "2.5rem")};
+  font-size: 2.5rem;
   color: ${({ color }) => color};
-`;
+  @media (max-width: 768px) {
+    font-size: ${({ isLast }) => (isLast ? "2rem" : "2.5rem")};
+  }
+`
 
 const CardText = styled.div`
   font-size: 1.2rem;
   margin-top: 0.5rem;
   color: ${({ color }) => color};
-`;
+`
 
 const CardInfo = styled.div`
   font-size: 1rem;
   margin-top: 0.3rem;
   color: ${({ color }) => color};
-`;
+`
 
-export default FlipCard;
+export default FlipCard
