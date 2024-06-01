7bcfb4ec83277d260dc1b244c85c7de28a8475d2
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-06-01 20:49:37 +0600

    [updated]ok

--- src/Components/DetailedInvoice/DetailedInvoice.component.jsx
+++ src/Components/DetailedInvoice/DetailedInvoice.component.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import styled from "styled-components"
-import { useParams, useNavigate } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import Header from "../SharedComponents/Header/Header"
 // import logo from '../../Assets/Logo/ParkSpotterLogoBlack.svg'
 import { ParkSpotterLogoBlack } from "../../Assets/Logo/Logo"
